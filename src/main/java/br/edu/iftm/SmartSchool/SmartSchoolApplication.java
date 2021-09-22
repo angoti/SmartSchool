2f1cf8c7b7745fc3f6a38f2567000a450da6a413
Ezequiel <zeque2020@gmail.com>
2021-09-22 19:59:41 -0300

    Inicia Projeto SmartSchool

---
+++ src/main/java/br/edu/iftm/SmartSchool/SmartSchoolApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.SmartSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSchoolApplication.class, args);
+	}
+
+}
